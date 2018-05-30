bbae2fc7868073bd0dcb1011bb553627f4ed6f16
Tim Broder <tim@kidfund.us>
2018-05-30 12:37:12 -0400

    strip out nav component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NotFound from 'app/scenes/not-found';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Button } from 'reactstrap';
+
 // import 'bootstrap/dist/css/bootstrap.css';
 import bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
@@ -10,7 +23,38 @@ import { setGlobalCssModule } from 'reactstrap/lib/utils';
 setGlobalCssModule(bootstrap);
 
 const App = (
-  <NotFound />
+  <Navbar color="light" light expand="md">
+    <Button color="danger">Danger!</Button>
+    <NavbarBrand href="/">reactstrap</NavbarBrand>
+    <NavbarToggler onClick={this.toggle} />
+    <Collapse navbar>
+      <Nav className="ml-auto" navbar>
+        <NavItem>
+          <NavLink href="/components/">Components</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+        </NavItem>
+        <UncontrolledDropdown nav inNavbar>
+          <DropdownToggle nav caret>
+            Options
+          </DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem>
+              Option 1
+            </DropdownItem>
+            <DropdownItem>
+              Option 2
+            </DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem>
+              Reset
+            </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </Nav>
+    </Collapse>
+  </Navbar>
 );
 
 ReactDOM.render(App, document.getElementById('root'));
